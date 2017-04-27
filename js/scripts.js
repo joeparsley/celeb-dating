ec4f3c7a6388825eb5b521703253cc344a4106c1
Daniel Lopez and Joe Parsley <joe@>
2017-04-27 13:29:08 -0700

    finished HTML, working Javascrip, no styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
+
+// Front-End Logic
 $(document).ready(function() {
   $("#personal-info").submit(function(event) {
     var ageInput = parseInt($("input#age").val());
     var genderInput = $("#gender").val();
     var hairColorInput = $("#hair-color").val();
-    event.preventDefault();
 
-    console.log(ageInput);
+    if ( genderInput === "female" && ageInput < 30 && hairColorInput != "other") {
+      $(".init-hidden").hide();
+      $("#khal").show();
+    } else {
+      $(".init-hidden").hide();
+      $("#dragon").show();
+    }
+    if ( genderInput === "male" && ageInput < 30 && hairColorInput != "other") {
+      $(".init-hidden").hide();
+      $("#danny").show();
+    } else {
+      $(".init-hidden").hide();
+      $("#dragon").show();
+    }
+    event.preventDefault();
   });
 });
